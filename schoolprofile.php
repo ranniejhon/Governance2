32650c958a55b42382f994494df2caa01d05c764
blair015 <tblairbrian@gmail.com>
2023-09-27 10:38:56 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -70,7 +70,10 @@ mysqli_close($conn);
             <?php endif; ?>
             <br></br>
             <h5 class="author-card-name" style="font-size: 50px;"><?php echo $school_address; ?></h5>
-            <span class="author-card-position" style="font-size: 20px; "><?php echo $school_name; ?></span>
+            <span class="author-card-position" style="font-size: 20px; "><?php echo $school_name; ?></span><br>
+            <span class="author-card-position" style="font-size: 20px; color: blue; font-style: italic; cursor: pointer;" data-toggle="modal" data-target="#updateProfileModal">
+              <i class="fas fa-edit"></i> Edit Profile
+</span>
         </div>
     </div>
 </div>
@@ -344,7 +347,147 @@ if (isset($_GET['school_id'])) {
   <!-- /.content-wrapper -->
 
 
+  <form action="save_school.php" method="POST">
+<div class="modal fade" id="updateProfileModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Add School</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+        <?php
+        include("admin/config/dbcon2.php");
 
+        $sql = "SELECT * FROM schools";
+        $sql2 = mysqli_query($conn, $sql);
+
+        ?>
+        <div class="row mb-4">
+            <div class="col-md-6">
+                <label class="fw-bold text-muted"><h5>School ID</h5></label>
+                <select class="form-select form-select-solid" data-control="select2" data-hide-search="true" data-placeholder="Select ID" id="school_id" name="school_id" class="form-control">
+                    <option value="">Select School ID</option>
+                    <?php
+                    if (mysqli_num_rows($sql2) > 0) {
+                        foreach ($sql2 as $row) {
+                            ?>
+                            <option value="<?= $row['school_id'] ?>"><?= $row['school_id'] ?></option>
+                        <?php
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+            <form action="code.php" method="POST">	
+										<div class="card-body p-9">
+											<!--begin::Row-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-bold text-muted"><h5>Upload School Logo</h5></label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8">
+													<!--begin::Image input-->
+													<div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url(assets/media/avatars/blank.png)">
+																<!--begin::Preview existing avatar-->
+																<div class="image-input-wrapper w-150px h-150px" style="background-image: url(assets/media/avatars/deped.png)"></div>
+																<!--end::Preview existing avatar-->
+																<!--begin::Label-->
+																<label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+																	<i class="bi bi-pencil-fill fs-7"></i>
+																	<!--begin::Inputs-->
+																	<input type="file" name="school_logo" accept=".png, .jpg, .jpeg" value =""  />
+																	<input type="hidden" name="avatar_remove" />
+																	<!--end::Inputs-->
+																</label>
+																<!--end::Label-->
+															</div>
+															<!--end::Image input-->
+														</div>
+														<!--end::Col-->
+													</div>
+													<!--end::Row-->
+
+													<div class="row mb-7">
+														<!--begin::Label-->
+														<label class="col-lg-4 fw-bold text-muted"><h5>Upload School Header</h5></label>
+														<!--end::Label-->
+														<!--begin::Col-->
+														<div class="col-lg-8">
+															<!--begin::Image input-->
+															<div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url(assets/media/avatars/blank.png)">
+																<!--begin::Preview existing avatar-->
+																<div class="image-input-wrapper w-550px h-200px" style="background-image: url(assets/media/avatars/header.jpg)"></div>
+																<!--end::Preview existing avatar-->
+																<!--begin::Label-->
+																<label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+																	<i class="bi bi-pencil-fill fs-7"></i>
+																	<!--begin::Inputs-->
+																	<input type="file" name="school_header" accept=".png, .jpg, .jpeg" value =""  />
+																	<input type="hidden" name="avatar_remove" />
+																	<!--end::Inputs-->
+																</label>
+																<!--end::Label-->																
+															</div>
+															<!--end::Image input-->
+												</div>
+												<!--end::Col-->
+											</div>
+            <div class="col-md-6">
+                <label class="fw-bold text-muted"><h5>School Name</h5></label>
+                <input class="form-control form-control-solid" size="5" type="text" class="form-control" name="school_name" id="school_name" readonly>
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col-md-6">
+                <label class="fw-bold text-muted"><h5>School Address</h5></label>
+                <input class="form-control form-control-solid" size="5" type="text" class="form-control" name="school_address">
+            </div>
+            <div class="col-md-6">
+                <label class="fw-bold text-muted"><h5>District</h5></label>
+                <input class="form-control form-control-solid" size="5" type="text" class="form-control" name="district" id="district" readonly>
+            </div>
+        </div>
+        <div class="row mb-4">
+        <div class="col-md-6">
+                <label class="fw-bold text-muted"><h5>Email Address:</h5></label>
+                <input class="form-control form-control-solid" size="5" type="text" class="form-control" name="email_address">
+            </div>
+            <div class="col-md-6">
+                <label class="fw-bold text-muted"><h5>School Size</h5></label>
+                <select class="form-select form-select-solid" data-control="select2" data-hide-search="true" data-placeholder="Select Category" name="category" class="form-control">
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="very_large">Very Large</option>
+                </select>
+            </div>
+            <div aria-live="polite" aria-atomic="true" style="position: fixed; top: 20px; right: 20px;">
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <strong class="mr-auto">Success</strong>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            <!-- Toast message content goes here -->
+        </div>
+    </div>
+</div>
+
+        </div>
+    </form>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    <button type="submit" class="btn btn-primary" name="save_changes">Save changes</button>
+</div>
+</div>
 
 
 
